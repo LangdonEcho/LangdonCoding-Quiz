c243cf47cbec66a24757a8a0728eb4054e979c5d
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-10-24 23:17:18 -0500

    adding most of the Html and Css

---
+++ script.js
@@ -0,0 +1,5 @@
+const question = Document.querySelector('#question');
+const Answers = Array from (Document.querySelectorAll('.Answer-text');
+const question = Document.querySelector('#question');
+const question = Document.querySelector('#question');
+const question = Document.querySelector('#question');
