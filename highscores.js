2f0612581c0322359c47a0b8a6fe98d56c0436ab
LangdonEcho <86031485+LangdonEcho@users.noreply.github.com>
2021-12-09 19:06:34 -0600

    continue to fix and working on the timer

--- highscores.js
+++
@@ -1,9 +0,0 @@
-// target id and declaring variables
-var highScoreList = document.querySelector('#highScoresList')
-
-var highScores = JSON.parse(localStorage.getItem("highScores")) || []
-//creating new array 
-highScoresList.innerHTML = highScores.map(score => {
-//showing name and score joined 
-    return `<li class="high-score">${score.name} - ${score.score}</li>`
-}).join("")
